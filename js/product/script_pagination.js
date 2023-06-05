55b32ece3d0d6b0dc46739be0e58ff3c9be8200c
PAVEL <mindalev77@mail.ru>
2023-06-05 14:04:33 +0300

    17 pagination v2

--- js/product/script_pagination.js
+++ js/product/script_pagination.js
@@ -1,1 +1,16 @@
 
+$(document).ready(function() {
+    var $listItems = $("ul li");
+    var $activeItem = $("ul li a.active");
+    var activeIndex = $listItems.index($activeItem.parent());
+    var maxVisibleItems = $(window).width() < 1100 ? 3 : 5;
+    var startIndex = Math.max(0, activeIndex - Math.floor(maxVisibleItems / 2));
+    var endIndex = Math.min(startIndex + maxVisibleItems - 1, $listItems.length - 1);
+
+    $listItems.hide();
+    $listItems.slice(startIndex, endIndex + 1).show();
+
+    if (endIndex < $listItems.length - 1) {
+        $("<li class='pagination__next'><a href='#'>Следующий<img src='img/vectorRight.svg'></a></li>").insertAfter($listItems.eq(endIndex));
+    }
+});
